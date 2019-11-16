2f60fb99a62d617451d7a2a9c532880d595d28a7
Hir0ki <david.scheiklies@protonmail.com>
2019-11-16 22:27:16 +0100

    add mecanum_contoller file for the MecanumContoller class

---
+++ src/test/test_mecanum_contoller.py
@@ -0,0 +1,10 @@
+import pytest
+from src.mecanum_controller import MecaniumController
+
+
+def test_mecanum_exists():
+    test = MecaniumController
+    assert test == MecaniumController
+
+
+def test_
